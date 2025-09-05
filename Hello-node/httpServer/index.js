5e9d1a99212b2d3a7cae1a578ed6df6e3cd1296d
Tgaurav1k <tgaurav1k@gmail.com>
2025-09-05 10:52:17 +0530

    Initial commit: Add Node.js project files

--- Hello-node/httpServer/index.js
+++
@@ -1,8 +0,0 @@
-// import http for creating web server
-const  http = require("http");
-
-// create web server
-const myServer = http.createServer((req,res)=>{
-  console.log("New req recorded");
-  res.end("Hello from server");
-});
