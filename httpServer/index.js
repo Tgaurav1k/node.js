c608ca806a92848f385991d1d8c2b8b0858c934c
Tgaurav1k <tgaurav1k@gmail.com>
2025-09-11 13:03:43 +0530

    Expand httpServer/index.js with routing, logging, and signup handling

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,8 +1,42 @@
 // import http for creating web server
 const  http = require("http");
+const fs = require("fs");
+const url = require("url");
+const { verify } = require("crypto");
+
 
 // create web server
 const myServer = http.createServer((req,res)=>{
-  console.log("New req recorded");
-  res.end("Hello from server");
+   if(req.url === "/favicon.ico" ) return res.end();
+   const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
+   const myUrl = url.parse(req.url, true);
+  // console.log(myUrl);
+  fs.appendFile("httpReqData.txt",log, (err,data)=>{
+    switch (myUrl.pathname){
+      case "/":
+        // req for get 
+        if(req.method === "GET")
+        res.end("Home Page")
+        break;
+        case "/about":
+          const userName = myUrl.query.myName;
+          res.end(`Hi, ${userName}`);
+          break;
+        case "/search":
+       const search = myUrl.query.search_query;   
+        res.end("Here are the result for"+ search);
+        case '/signup':
+        if(req.method === 'GET')
+          res.end("This is a signup Form");
+        else if ( req.method=== "POST"){
+          // DB query run 
+          res.end("Success");
+        }
+        default:
+          res.end("404 Not Found");
+
+    }
+  });
 });
+
+myServer.listen(8000, ()=>console.log("server started"))
