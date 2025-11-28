43ac560ac0b970b0468e9cbb8de93f5dbd9d5382
Gaurav kumar <tgaurav1k@gmail.com>
2025-11-28 15:00:21 +0530

    everthign new posted

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const users = require("./MOCK_DATA.json")
+const users = require("./MOCK_DATA.json") 
 const app = express();
 const PORT = 8080;
 
@@ -7,10 +7,31 @@ const PORT = 8080;
 app.get("/api/users", (req, res)=>{
     return res.json(users)
 })
+
+// REST API     // get request 
+app.get("/api/users/:id", (req, res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id === id );
+    return res.json(user);
+})
+.patch((req,res)=>{
+    // Edit usr with id 
+    return res.json({ status: "Pending"});
+})
+.delete((req, res)=>{
+    // delete user with id 
+    return res.json({status: "Pending"})
+});
+// app.get("/api/users/:id", (req, res)=> {
+//     const id = Number(req.params.id);
+//     const user = users.find((user)=> user.id === id);
+//     return res.json(user);
+// });
+
 app.get("/users" , (req,res)=>{
     const html = `
     <ul>
-    ${users.map((user)=> 
+    ${users.map((user)=>
     `<li>${user.first_name}</li>`
     ).join("")}
     </ul>
@@ -18,15 +39,12 @@ app.get("/users" , (req,res)=>{
     res.send(html);
 })
 
+// POST new user 
 
-app.get("/users", (req,res)=>{
-    const html = `
-    <ul> 
-    ${users.map((user)=> `<li>${user.first_name}</li>` )}
-    </ul>
-    ` ;
-    res.send(html);
-})
+app.post("/api/users", (req,res)=>{
+
+} )
+
 
 app.listen(PORT, ()=>{
     console.log(`Server stated at ${PORT}`)
